1191264c8ea939e9b204faae8de0933332390baa
Nwafor Emmanuel <emmanuelnwaforce@gmail.com>
2025-03-03 15:29:29 +0100

    pointer

--- AdminPanel/src/Header.jsx
+++ AdminPanel/src/Header.jsx
@@ -1,24 +1,45 @@
-import React from 'react'
-import 
- {BsFillBellFill, BsFillEnvelopeFill, BsPersonCircle, BsSearch, BsJustify}
- from 'react-icons/bs'
+import React, { useState } from 'react';
+import { 
+  BsFillBellFill, 
+  BsFillEnvelopeFill, 
+  BsPersonCircle, 
+  BsSearch, 
+  BsJustify 
+} from 'react-icons/bs';
 
-function Header({OpenSidebar}) {
+function Header({ OpenSidebar }) {
+  const [showSearchBar, setShowSearchBar] = useState(false);
+  const [searchQuery, setSearchQuery] = useState('');
+
   return (
-    <header className='header'>
-        <div className='menu-icon'>
-            <BsJustify className='icon' onClick={OpenSidebar}/>
-        </div>
-        <div className='header-left'>
-            <BsSearch  className='icon'/>
-        </div>
-        <div className='header-right'>
-            <BsFillBellFill className='icon'/>
-            <BsFillEnvelopeFill className='icon'/>
-            <BsPersonCircle className='icon'/>
-        </div>
+    <header className="header">
+      <div className="menu-icon">
+        <BsJustify className="icon" onClick={OpenSidebar} />
+      </div>
+
+      <div className="header-left">
+        {showSearchBar ? (
+          <input
+            type="text"
+            className="search-input"
+            placeholder="Search..."
+            value={searchQuery}
+            onChange={(e) => setSearchQuery(e.target.value)}
+            onBlur={() => setShowSearchBar(false)} // Hide input when clicked outside
+            autoFocus
+          />
+        ) : (
+          <BsSearch className="icon" onClick={() => setShowSearchBar(true)} />
+        )}
+      </div>
+
+      <div className="header-right">
+        <BsFillBellFill className="icon" />
+        <BsFillEnvelopeFill className="icon" />
+        <BsPersonCircle className="icon" />
+      </div>
     </header>
-  )
+  );
 }
 
-export default Header
+export default Header;
