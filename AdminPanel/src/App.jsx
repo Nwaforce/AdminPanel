1686f8b4fbea265ac0d9f729040c287f272331ed
Nwafor Emmanuel <emmanuelnwaforce@gmail.com>
2025-03-03 15:07:21 +0100

    import

--- AdminPanel/src/App.jsx
+++ AdminPanel/src/App.jsx
@@ -1,10 +1,23 @@
-import { useState } from "react";
-import "./App.css";
+import { useState } from 'react'
+import './App.css'
+import Header from './Header'
+import Sidebar from './Sidebar'
+import Home from './Home'
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [openSidebarToggle, setOpenSidebarToggle] = useState(false)
 
-  return <></>;
+  const OpenSidebar = () => {
+    setOpenSidebarToggle(!openSidebarToggle)
+  }
+
+  return (
+    <div className='grid-container'>
+      <Header OpenSidebar={OpenSidebar}/>
+      <Sidebar openSidebarToggle={openSidebarToggle} OpenSidebar={OpenSidebar}/>
+      <Home />
+    </div>
+  )
 }
 
-export default App;
+export default App
